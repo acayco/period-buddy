445ee151935ad8d7ac20654cc96ccf3f5714d181
elliespring <91167045+elliespring@users.noreply.github.com>
2021-12-10 18:26:31 -0500

    commit master

--- scripts/homescripts.js
+++ scripts/homescripts.js
@@ -1,21 +1,4 @@
-const mainMenu = document.querySelector(".mainMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const openMenu = document.querySelector(".openMenu");
-
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
-
-function show() {
-  mainMenu.style.display = "flex";
-  mainMenu.style.top = "0";
-}
-
-function close() {
-  mainMenu.style.top = "-100%";
-}
-
-
-(function () {
+(function  () {
   var carouselTransition, carouselContent, carouselIndex, carouselLength, firstClone, firstItem, isAnimating, itemWidth, lastClone, lastItem;
   carouselTransition = 250;
   carouselContent = $('.carousel__content');
@@ -29,7 +12,7 @@ function close() {
   firstClone = null;
   lastClone = null;
   carouselContent.css('width', carouselLength * 100 + '%');
- carouselContent.transition({ x: carouselIndex * -itemWidth + '%' }, 0);
+  carouselContent.transition({ x: carouselIndex * -itemWidth + '%' }, 0);
   $.each(carouselContent.children(), function () {
       return $(this).css('width', itemWidth + '%');
   });
@@ -73,36 +56,16 @@ function close() {
   });
 }.call(this));
 
-
 function calc_results() {
 var count;
 $(".questions").each(function(){
-  if(this.is(":checked")) {
+  if(this.is(":checked")) {s
     count++;
   }
 });
 }
 
 
-
-var results = [
-["13.33", "5.45", "2.68"],    // 0
-["8.89", "4.55", "9.82"],     // 1
-["5.56", "4.55", "14.29"],    // 2
-["8.89", "4.55", "0.00"],     // 3
-["1.11", "6.36", "6.25"],     // 4
-["11.11", "3.64", "4.46"],    // 5
-["8.89", "4.55", "4.46"],     // 6
-["5.56", "4.55", "6.25"],     // 7
-["5.56", "4.55", "2.68"],     // 8
-["6.67", "8.18", "8.93"],     // 9
-["10.00", "10.00", "6.25"],   // 10
-["3.33", "5.45", "4.46"],     // 11
-["3.33", "5.45", "8.04"],     // 12
-["4.44", "11.82", "4.46"],    // 13
-["2.22", "10.00", "2.68"],    // 14
-["1.11", "6.36", "14.29"]     // 15
-]
 
 function sumArray(from, to, size) {
 var sum = parseFloat("0.00");
@@ -128,38 +91,10 @@ $(".questions").each(function() {
       count++;
   }
 });
-
-
-
-
 var less = sumArray(0, count-1, size) ;
 var same = sumArray(count, count, size);
 var more = sumArray(count+1, 15, size);
 
-var resultsText = "";
-resultsText += '<div class="container">';
-resultsText += '<h2 style="margin-top: 0.5em; color: #00a5b5;">So, Where Do You Stand Today?</h2>';
-resultsText = resultsText + '<h3>You are currently automating <strong>' + (count) + '</strong> controls of the 15 sub-controls used to assess Critical Security Controls 1 - 5 in your organization of <strong>' + $("input[name=size]:checked").attr('data-value') + '</strong> people.</h3>';
-resultsText += '<div style="margin-top: 2.5em;" class="score-slider-track">';
-resultsText += '<div class="score-slider">0%</div>';
-resultsText += '<div class="score-slider-less">0%</div>';
-resultsText += '<div class="score-slider-more">0%</div>';
-resultsText += '</div>';
-resultsText += '<div class="flex-grid mb">';
-resultsText += '<div class="flex-two flex-fewer">';
-resultsText = resultsText + '<p style="margin: 0; font-size: 4em;">' + less + "%" + '</p>';
-resultsText += '<p style="margin: 0;">of similar sized organizations have automated <strong>fewer</strong> controls</p>';
-resultsText += '</div>';
-resultsText += '<div class="flex-two flex-same">';
-resultsText = resultsText + '<p style="margin: 0; font-size: 5em;">' + same + "%" + '</p>';
-resultsText += '<p style="margin: 0;">of similar sized organizations have automated the <strong>same</strong> number of controls</p>';
-resultsText += '</div>';
-resultsText += '<div class="flex-two flex-more">';
-resultsText = resultsText + '<p style="margin: 0; font-size: 4em;">' + more + "%" + '</p>';
-resultsText += '<p style="margin: 0;">of similar sized organizations have automated <strong>more</strong> controls</p>';
-resultsText += '</div>';
-resultsText += '</div>';
-resultsText += '</div>';
 
 $("#auto-con-calc").html(resultsText);
 sliderControl(less,same,more);
@@ -169,8 +104,6 @@ $("#auto-con-calc").slideDown("fast", function() {
   }, 'slow');
 });
 }
-
-
 
 function update_progress_bar(index) {
 var checked = index;
@@ -195,17 +128,378 @@ $('.nav--buttons--right').trigger('click');
 });
 
 function sliderControl(less, same, more) {
-  var less_slider = $(".score-slider-less");
-  var same_slider = $(".score-slider");
-  var more_slider = $(".score-slider-more");
+var less_slider = $(".score-slider-less");
+var same_slider = $(".score-slider");
+var more_slider = $(".score-slider-more");
 
-    if ( same < 5.00 ) {
-      same_slider.css("width", "5%");
-    }
-    else {
-      less_slider.css("width", same + "%");
-    }
-    same_slider.css("left", less + "%").html(same + "%");
-    less_slider.css("width", (parseFloat(less) + (parseFloat(same) / 2)) + "%").html(less + "%");
-    more_slider.css("width", (parseFloat(more) + (parseFloat(same) / 2)) + "%").html(more + "%");
+if ( same < 5.00 ) {
+  same_slider.css("width", "5%");
 }
+else {
+  less_slider.css("width", same + "%");
+}
+same_slider.css("left", less + "%").html(same + "%");
+less_slider.css("width", (parseFloat(less) + (parseFloat(same) / 2)) + "%").html(less + "%");
+more_slider.css("width", (parseFloat(more) + (parseFloat(same) / 2)) + "%").html(more + "%");
+}
+
+
+const productData = [
+  {
+   name: "Always Thin Daily Liners",
+   type:"pantyLiner",
+   price:"low",
+   flow:"lightFlow",
+   use:"external",
+   material:"nonOrganic",
+   reusability:"singleUse",
+   difficulty:"easy",
+   mobility:"null",
+   img:"images/pantyliners-always.png",
+   url:"pantyliners_always.html"
+ },
+ {
+    name:"Rael Organic Cotton Panty Liners",
+    type:"pantyLiner",
+    price:"moderate",
+    flow:"lightFlow",
+    use:"external",
+    reusability:"singleUse",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pantyliners-rael.html",
+    img: "images/pantyliners-rael.png",
+  },
+  {
+    name: "Rael Organic Cotton Reusable Panty Liners",
+    type:"pantyLiner",
+    price:"high",
+    use:"external",
+    flow:"lightFlow",
+    reusability:"reusable",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pantyliners-reusable-rael.html",
+    img: "images/pantyliners-reusable-rael.png",
+  },
+  {
+   name: "Always Ultra Thin Pads",
+   type:"maxiPad",
+   price:"low",
+   use:"external",
+   flow:"moderateFlow",
+   reusability: "singleUse",
+   material:"nonOrganic",
+   difficulty:"easy",
+   mobility:"null",
+   url: "pads-regular-always.html",
+   img: "images/pads-regular-always.png", 
+  },
+  {
+    name: "Rael Organic Cotton Pads (Regular)",
+    type:"maxiPad",
+    price:"moderate",
+    use:"external",
+    flow:"moderateFlow",
+    reusability:"singleUse",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-regular-rael.html",
+    img: "images/pads-regular-rael.png"
+  },
+  {
+    name: "Rael Organic Cotton Reusable Pads (Regular)",
+    type:"maxiPad",
+    price:"high",
+    use:"external",
+    flow:"moderateFlow",
+    reusability: "reusable",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-regular-reusable-rael.html",
+    img: "images/pads-reusable-rael.png"
+  },
+  {
+    name:"Always Pure Cotton with Wings",
+    type:"maxiPad",
+    price:"low",
+    use:"external",
+    flow:"heavyFlow",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-heavy-always.html",
+    img: "images/pads-heavy-always.png",
+  },
+  {
+    name:"Rael Organic Cotton Pads (Large)",
+    type:"maxiPad",
+    price:"moderate",
+    use:"external",
+    flow:"heavyFlow",
+    reusability:"singleUse",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-heavy-rael.html",
+    img: "images/pads-regular-rael.png",
+  },
+  {
+    name: "Rael Organic Cotton Reusable Pads (Large)",
+    type:"maxiPad",
+    price:"high",
+    use:"external",
+    flow:"heavyFlow",
+    reusability: "reusable",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-heavy-reusable-rael.html",
+    img: "images/pads-reusable-rael.png",
+  },
+  {
+    name: "Always Overnight Absorbency Unscented without Wings",
+    type:"maxiPad",
+    price:"low",
+    use:"external",
+    flow:"superHeavy",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-superheavy-always.html",
+    img: "images/pads-super-heavy-always.png",
+  },
+  {
+    name: "Rael Organic Cotton Overnight Pads",
+    type:"maxiPad",
+    price:"moderate",
+    use:"external",
+    flow:"superHeavy",
+    reusability:"singleUse",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-superheavy-rael.html",
+    img: "images/pads-super-heavy-rael.png",
+  },
+  {
+    name: "Rael Organic Cotton Reusable Pads (Overnight)",
+    type:"maxiPad",
+    price:"high",
+    use:"external",
+    flow:"superHeavy",
+    reusability:"reusable",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "pads-superheavy-reusable-rael.html",
+    img: "images/pads-super-heavy-reusable-rael.png",
+  },
+  {
+    name: "Tampax Lite Absorbency Tampons",
+    type:"tampon",
+    price:"low",
+    use:"internal",
+    flow:"lightFlow",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-light-tampax.html",
+    img: "images/tampons-light-tampax.png",
+  },
+  {
+    name: "Tampax Regular Absorbency with LeakGuard Braid",
+    type:"tampon",
+    price:"low",
+    use:"internal",
+    flow:"moderateFlow",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-regular-tampax.html",
+    img: "images/tampons-regular-tampax.png",
+  },
+  {
+    name: "Rael Organic Tampons (Regular)",
+    type:"tampon",
+    price:"moderate",
+    use:"internal",
+    flow:"moderateFlow",
+    reusability: "singleUse",
+    material:"organic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-regular-rael.html",
+    img: "images/tampons-organic-rael.png",
+  },
+  {
+    name: "Tampax Ultra Tampons",
+    type:"tampon",
+    price:"low",
+    use:"internal",
+    flow:"heavyFlow",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-heavy-tampax.html",
+    img: "images/tampons-heavy-tampax.png",
+  },
+  {
+    name: "Rael Organic Tampons (Super)",
+    type:"tampon",
+    price:"moderate",
+    use:"internal",
+    flow:"heavyFlow",
+    reusability: "singleUse",
+    material:"organic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-heavy-rael.html",
+    img: "images/tampons-heavy-rael.png",
+  },
+  {
+    name: "Tampax Super Plus Absorbency Tampons",
+    type:"tampon",
+    price:"low",
+    use:"internal",
+    flow:"superHeavy",
+    reusability: "singleUse",
+    material:"nonOrganic",
+    difficulty:"medium",
+    mobility:"active",
+    url: "tampons-superheavy-tampax.html",
+    img: "images/tampons-super-heavy-tampax.png",
+  },
+  {
+    name: "Rael Reusable Menstrual Cup (Size 1)",
+    type: "menstrualCup",
+    price:"high",
+    use:"internal",
+    flow:"moderateFlow",
+    reusability: "reusable",
+    material:"nonOrganic",
+    difficulty:"difficult",
+    mobility:"active",
+    url: "menstrualcups-rael.html",
+    img: "images/menstrualcup-size1.png",
+  },
+  {
+    name: "Rael Reusable Menstrual Cup (Size 2)",
+    type: "menstrualCup",
+    price:"high",
+    use:"internal",
+    flow:"heavyFlow",
+    reusability: "reusable",
+    material:"nonOrganic",
+    difficulty:"difficult",
+    mobility:"active",
+    url: "menstrualcups-heavy-rael.html",
+    img: "images/menstrualcup-size2.png",
+  },
+  {
+    name: "Rael Organic Cotton Disposable Period Underwear",
+    type: "periodUnderwear",
+    price:"moderate",
+    use:"external",
+    flow:"heavyFlow",
+    reusability: "singleUse",
+    material:"organic",
+    difficulty:"easy",
+    mobility:"null",
+    url: "periodunderwear-rael.html",
+    img: "images/periodunderwear-disposable-rael.png",
+  },
+  {
+    name: "Rael Reusable Period Underwear",
+    type: "periodUnderwear",
+    price:"high",
+    use:"external",
+    flow:"heavyFlow",
+    reusability: "reusable",
+    material:"nonOrganic",
+    difficulty:"easy",
+    mobility:"active",
+    url: "periodunderwear-reusable-rael.html",
+    img: "images/periodunderwear-reusable-real.png",
+  }
+]
+function compareArr(arr1, object1)
+{
+  const intersection = arr1.filter(element =>
+Object.values(object1).includes(element));
+  return intersection.length;
+}
+let newUser = [];
+let btn = document.getElementById("btnCalculate");
+
+btn.addEventListener("click", ( )=>
+   {
+    let questionOne = document.getElementsByName("questionOne");
+  let questionTwo = document.getElementsByName("questionTwo");
+  let questionThree = document.getElementsByName("questionThree");
+  let questionFour = document.getElementsByName("questionFour");
+  let questionFive = document.getElementsByName("questionFive");
+  let questionSix = document.getElementsByName("questionSix");
+  let questionSeven = document.getElementsByName("questionSeven");
+    questionOne.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+      questionTwo.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+  questionThree.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+  questionFour.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+  questionFive.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+  questionSix.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+  questionSeven.forEach((question)=>{
+      if (question.checked) {
+        newUser.push(question.value);
+      }
+    });
+    console.log(newUser);
+    resultarray = [];
+    for(i = 0; i<productData.length; i++)
+      {
+        resultarray.push(compareArr(newUser, productData[i]));
+      }
+    maxIndex = resultarray.indexOf(Math.max.apply(Math, resultarray));
+    nameOfProduct = productData[maxIndex].name;
+    productURL = productData[maxIndex].url;
+    btn.setAttribute("href", productURL);
+    console.log(btn.href);
+    } );
+
+
+
+//calculate//
+
+///results///
